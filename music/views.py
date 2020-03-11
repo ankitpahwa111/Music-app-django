313c4ffea768144185100d64c08365b2ab055cac
ankit <ankitpahwa111@gmail.com>
2020-03-11 18:40:56 +0530

    activating links and models

--- music/views.py
+++ music/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1> Hi  BHosdike </h1>")
+
+def details(request, album_id):
+    return HttpResponse("<h2> The details for the page with id " + str(album_id) + " </h2>")
